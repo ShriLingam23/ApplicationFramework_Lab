bf911da29f1a0fb2bb11d2ed3cbd13e23ed50785
ShriLingam23 <trnshri23@gmail.com>
2019-05-25 02:31:11 +0530

    Update and delete implementation

--- Lab_05/app.js
+++ Lab_05/app.js
@@ -33,7 +33,58 @@ app.get('/user/:id',(req,res)=>{
    }
 })
 
+//Read All
+app.get('/all',(req,res)=>{
 
+    try{
+        res.status(200).send({message:"Users successfully retrieved",data:users})
+    }
+    catch(e){
+        res.status(500).send({message:"Error :"+e})
+    }
+})
+
+//Update
+app.put('/update/;id',(req,res)=>{
+
+    try{
+        const index = users.findIndex(instance=>{
+            return instance.id===parseInt(req.params.id)
+        })
+
+        if(index>-1){
+            users[index].fName=req.body.fName;
+            users[index].lName=req.body.lName;
+            users[index].dob=new Date(req.body.dob);
+            res.status(200).send({message:"User successfully updated",data:users[index]})
+        }
+        else
+            res.status(404).send({message:"User not updated"})
+    }
+    catch(e){
+        res.status(500).send({message:"Error :"+e})
+    }
+})
+
+//Delete
+app.delete('/update/;id',(req,res)=>{
+
+    try{
+        const index = users.findIndex(instance=>{
+            return instance.id===parseInt(req.params.id)
+        })
+
+        if(index>-1){
+            users.splice(index,1)
+            res.status(200).send({message:"User successfully deleted",data:users[index]})
+        }
+        else
+            res.status(404).send({message:"User not deleted"})
+    }
+    catch(e){
+        res.status(500).send({message:"Error :"+e})
+    }
+})
 
 app.listen(3000,(err)=>{
     console.log("Server listening at port :3000");
